ef5a2968ac5c6f611b7bc5854e0de43aa4a72dfb
Alexandre <aleaxe7@gmail.com>
2022-09-26 20:54:47 -0300

    Changed game main function, added restart game function, improve css

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
-let playerScore = 0
-let computerScore = 0
+let playerPoints = 0;
+let computerPoints = 0;
+let playerScore = document.getElementById("player-score");
+let computerScore = document.getElementById("computer-score");
+let buttons = document.querySelectorAll("input");
+let roundResult = document.getElementById("round-result");
+let finalResult = document.getElementById("final-result");
+
+
 
 //Get computer choice
 function getComputerChoice() {
@@ -11,6 +18,10 @@ function getComputerChoice() {
 
 // Play a single round
 function playRound(playerSelection) {
+    if (playerPoints >= 5 || computerPoints >= 5) {
+        restartGame()
+        return
+    }
     let computerSelection = getComputerChoice()
 
     if (playerSelection === computerSelection) {
@@ -18,22 +29,35 @@ function playRound(playerSelection) {
     } else if ((playerSelection === "Rock" && computerSelection === "Scissors") ||
         (playerSelection === "Paper" && computerSelection === "Rock") ||
         (playerSelection === "Scissors" && computerSelection === "Paper")) {
-            playerScore++
-            document.getElementById("player-score").innerHTML = playerScore
+            playerPoints++
+            playerScore.textContent = playerPoints
             return `You won! ${playerSelection.toUpperCase()} beats ${computerSelection.toUpperCase()}.`
     } else  if (playerSelection === "Fuck It") {
-        playerScore++
-        document.getElementById("player-score").innerHTML = playerScore
+        playerPoints++;
+        playerScore.textContent = playerPoints;
         return `Player just chose to F#%k the computer. You won!`
     }    
-    computerScore++
-    document.getElementById("computer-score").innerHTML = computerScore
+    computerPoints++;
+    computerScore.textContent = computerPoints;
     return `You lost! ${computerSelection.toUpperCase()} beats ${playerSelection.toUpperCase()}.`
 }   
-   
 
-let roundResult = document.getElementById("round-result")
-document.getElementById("rock-btn").addEventListener("click", () => roundResult.innerHTML = playRound("Rock"));
-document.getElementById("paper-btn").addEventListener("click", () => roundResult.innerHTML = playRound("Paper"));
-document.getElementById("scissors-btn").addEventListener("click", () => roundResult.innerHTML = playRound("Scissors"));
-document.getElementById("fck-btn").addEventListener("click", () => roundResult.innerHTML = playRound("Fuck It"));
+buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+        roundResult.textContent = playRound(button.value);
+        if (playerPoints >= 5) {
+            finalResult.textContent = "The player has won! Game Over!";
+        } else if (computerPoints >= 5) {
+            finalResult.textContent = "The computer has won! Game Over!";
+        }
+    })
+})
+
+function restartGame() {
+    playerPoints = 0;
+    computerPoints = 0;
+    playerScore.textContent = 0;
+    computerScore.textContent = 0;
+    roundResult.textContent = null;
+    finalResult.textContent = null;
+}
